2a1ec4b957f3e5365df191c01d10ed5449c3b6a2
Logan <lhoerth@mail.greenriver.edu>
2015-01-28 16:12:32 -0800

    Create style.css

---
+++ style.css
@@ -0,0 +1,70 @@
+.form-inline {
+    padding:10px;
+    margin-left: 3em;
+}
+
+.form-inline > * {
+    margin:10px 3px !important;
+}
+
+.container {
+    max-width: 700px; 
+    background-color: #ddd;
+    border-radius: 0 0 5px 5px;
+    padding-bottom: 1em;
+    padding-right: 3em;
+}
+
+body {
+    padding-left: 45px;
+    padding-right: 45px;
+}
+
+.degreeHighlights {
+    position:relative;
+}
+
+.degreeHighlights div {
+    display: none;
+}
+
+.degreeHighlightsHover {
+    position:relative;
+}
+
+.degreeHighlightsHover div {
+    display:block;
+    position: absolute;
+    width: 25em;
+    top:.3em;
+    top:5px;
+    left:5px;
+    box-shadow:5px 16px 16px 5px;
+    z-index:1000;
+    background-color: #DDD;
+    padding:3px;
+    border-radius:4px;
+}
+
+.category{
+    margin-left: 4em;
+    margin-bottom: 2em;
+    margin-top: 2em;
+}
+
+.col-md-4{
+    padding-top: 1.5em; 
+    padding-bottom: 1.5em;
+}
+
+#current {
+	padding-left: 2em;
+	display: none;
+}
+
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+    /* display: none; <- Crashes Chrome on hover */
+    -webkit-appearance: none;
+    margin: 0; /* <-- Apparently some margin are still there even though it's hidden */
+}
